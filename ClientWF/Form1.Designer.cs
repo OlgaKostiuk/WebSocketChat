b73198750a511b6d9aba8b65a8d57f3019eb9675
OlgaKostiuk <olga.d.kostiuk@gmail.com>
2017-10-26 23:11:51 +0300

    Client WinForm

---
+++ ClientWF/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace ClientWF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.inputTB = new System.Windows.Forms.TextBox();
+            this.ipTB = new System.Windows.Forms.TextBox();
+            this.ConnectBtn = new System.Windows.Forms.Button();
+            this.sendBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(546, 199);
+            this.listBox1.TabIndex = 0;
+            // 
+            // inputTB
+            // 
+            this.inputTB.Location = new System.Drawing.Point(189, 218);
+            this.inputTB.Name = "inputTB";
+            this.inputTB.Size = new System.Drawing.Size(289, 20);
+            this.inputTB.TabIndex = 1;
+            // 
+            // ipTB
+            // 
+            this.ipTB.Location = new System.Drawing.Point(12, 218);
+            this.ipTB.Name = "ipTB";
+            this.ipTB.Size = new System.Drawing.Size(90, 20);
+            this.ipTB.TabIndex = 2;
+            this.ipTB.Text = "localhost:9050";
+            // 
+            // ConnectBtn
+            // 
+            this.ConnectBtn.Location = new System.Drawing.Point(108, 216);
+            this.ConnectBtn.Name = "ConnectBtn";
+            this.ConnectBtn.Size = new System.Drawing.Size(75, 23);
+            this.ConnectBtn.TabIndex = 3;
+            this.ConnectBtn.Text = "Connect";
+            this.ConnectBtn.UseVisualStyleBackColor = true;
+            // 
+            // sendBtn
+            // 
+            this.sendBtn.Location = new System.Drawing.Point(484, 216);
+            this.sendBtn.Name = "sendBtn";
+            this.sendBtn.Size = new System.Drawing.Size(75, 23);
+            this.sendBtn.TabIndex = 4;
+            this.sendBtn.Text = "Send";
+            this.sendBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 261);
+            this.Controls.Add(this.sendBtn);
+            this.Controls.Add(this.ConnectBtn);
+            this.Controls.Add(this.ipTB);
+            this.Controls.Add(this.inputTB);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox inputTB;
+        private System.Windows.Forms.TextBox ipTB;
+        private System.Windows.Forms.Button ConnectBtn;
+        private System.Windows.Forms.Button sendBtn;
+    }
+}
+
---
+++ ClientWF/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientWF
+{
+    public partial class Form1 : Form
+    {
+        private ClientWebSocket client;
+        public Form1()
+        {
+            InitializeComponent();
+            sendBtn.Click += SendClick;
+            ConnectBtn.Click += ConnectClick;
+        }
+
+        private async void SendClick(object sender, EventArgs e)
+        {
+            try
+            {
+                byte[] buffer = Encoding.UTF8.GetBytes(inputTB.Text);
+                await client.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, false, CancellationToken.None);
+                listBox1.Items.Add("You: " + inputTB.Text);
+            }
+            catch
+            {
+                listBox1.Items.Add("Cannot connect to server!");
+            }
+        }
+
+        private async void ConnectClick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (client == null)
+                {
+                    client = new ClientWebSocket();
+                    await client.ConnectAsync(new Uri("ws://" + ipTB.Text), CancellationToken.None);
+                    listBox1.Items.Add("You've connected to the server!");
+                    Receiver();
+                }
+            }
+            catch (Exception ex)
+            {
+                listBox1.Items.Add("Cannot connect to server!");
+            }
+        }
+
+        private async void Receiver()
+        {
+            while (client.State == WebSocketState.Open)
+            {
+                byte[] buffer = new byte[1024];
+                var result = await client.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                listBox1.Items.Add("Server: " + Encoding.UTF8.GetString(buffer).TrimEnd('\0'));
+            }
+        }
+    }
+}
